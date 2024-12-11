0b774b7be73aa314230ccd6bf7251f22a7add01f
Diogo Muzzi <muzzidiogo@gmail.com>
2024-12-11 13:39:51 -0300

    Dia 05 Parte 2 Completa

--- 2024/dia05/dia05p2.py
+++ 2024/dia05/dia05p2.py
@@ -1,7 +1,8 @@
 from typing import List 
 from math import floor
 
-def check_rules(rules, update):
+def check_rules(rules: List[List[int]], update: List[int]):
+    changed = False
     for rule in rules:
         if rule[0] in update and rule [1] in update:
             i, j = update.index(rule[0]), update.index(rule[1])
@@ -10,19 +11,31 @@ def check_rules(rules, update):
                 temp2 = update[j] 
                 update[i] = temp2
                 update[j] = temp1
-                check_rules(update)
-    else: return update
+                changed = True
+    return update, changed
 
 def check_updates(rules:List[List[int]], updates:List[List[int]]) -> List[List[int]]:
     incorrectUpdates = []
     for update in updates:
-        update = check_rules(rules, update)
-        if incorrect: 
-            incorrectUpdates.append(update[floor(len(update)/2)])
-            print(update)
-    print('---')
-    print(incorrectUpdates)
-    return sum(incorrectUpdates)
+        incorrect = False
+        for rule in rules:
+            if rule[0] in update and rule [1] in update:
+                i, j = update.index(rule[0]), update.index(rule[1])
+                if i > j:
+                    temp1 = update[i]
+                    temp2 = update[j] 
+                    update[i] = temp2
+                    update[j] = temp1
+                    incorrect = True
+        if incorrect:
+            changed = False
+            new, changed = check_rules(rules, update)
+            while changed:
+                update = new
+                new, changed = check_rules(rules, update)
+            incorrectUpdates.append(update)
+
+    return incorrectUpdates
 
 def get_input(filepath:str) -> tuple[List[List[int]], List[List[int]]]:
     rules, updates = [], []
@@ -43,9 +56,17 @@ def get_input(filepath:str) -> tuple[List[List[int]], List[List[int]]]:
     return rules, updates
 
 def main() -> None:
-    input_path = 'demo.txt'
+    # [floor(len(update)/2)]
+    input_path = 'input.txt'
     rules, updates = get_input(input_path)
-    print(check_updates(rules, updates))
+    incorrectUpdates = check_updates(rules, updates)
+    print(incorrectUpdates)
+    
+    center = []
+    for update in incorrectUpdates:
+        center.append(update[floor(len(update)/2)])
+    
+    print(sum(center))
     
 if __name__ == '__main__':
     main()
